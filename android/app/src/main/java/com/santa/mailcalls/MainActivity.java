de888a3182d52e13ea3ccf7ef38df7194a7067f0
KIWYKL <kiwyjm1@gmail.com>
2025-12-02 10:57:39 -0400

    fix: add WebView microphone permission handler for Android

--- android/app/src/main/java/com/santa/mailcalls/MainActivity.java
+++ android/app/src/main/java/com/santa/mailcalls/MainActivity.java
@@ -1,5 +1,22 @@
 package com.santa.mailcalls;
 
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Enable WebView to request microphone permissions
+        this.bridge.getWebView().setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                // Automatically grant microphone permission
+                request.grant(request.getResources());
+            }
+        });
+    }
+}
